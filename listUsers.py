6794c71863e3fb6a49a53ec9dc1f4305c843eeba
Pierre Charles <pierre.charles@holidayextras.com>
2018-08-19 00:23:07 +0100

    This file lists users currently logged in every five seconds.

---
+++ listUsers.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+def loopF():
+        os.system('who')
+        time.sleep(5)
+
+while True:
+        print
+        date()
+        print("Users currently logged in:")
+        loopF()
+        print
+
